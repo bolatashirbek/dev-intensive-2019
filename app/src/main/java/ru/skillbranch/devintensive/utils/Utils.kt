53f86b5ba239b55a2038a2d5e7c0e91accd98518
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-07-03 12:34:48 +0600

    Реализовал утилитный метод parseFullName(fullName)

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,4 +1,23 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.replaceAll(" ", " ")?.split(" ")
+
+        val firstName = parts?.notEmptyOrNullAt(0)
+        val lastName = parts?.notEmptyOrNullAt(1)
+        return firstName to lastName
+    }
+
+    private fun List<String>.notEmptyOrNullAt(index: Int) = getOrNull(index).let {
+        if (it == "") null else it
+    }
+
+    private fun String.replaceAll(oldName: String, newName: String): String {
+        var value = this
+        while (value.contains(oldName)) {
+            value = value.replace(oldName, newName)
+        }
+        return value
+    }
 }
