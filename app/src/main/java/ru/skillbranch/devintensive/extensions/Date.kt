8f3563380b450acbe7c0ff52e65c43a974e349c2
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-07-03 16:08:13 +0600

    Реализовал extension метод для форматирования вывода даты Date.add

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -8,3 +8,15 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    time += value * units.value
+    return this
+}
+
+enum class TimeUnits(val value: Long) {
+    SECOND(1000L),
+    MINUTE(60 * SECOND.value),
+    HOUR(60 * MINUTE.value),
+    DAY(24 * HOUR.value)
+}
