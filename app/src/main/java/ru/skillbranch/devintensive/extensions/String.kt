76ec7ae1a58c893cfaa0cdf9519b760f6f809697
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-07-10 18:58:28 +0600

    Реализовал метод stripHtml для очистки строки от лишних пробелов, html тегов, escape последовательностей

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,3 +5,8 @@ fun String.truncate(size: Int = 16): String {
     if (result.length <= size) return result
     return result.substring(0, size).trimEnd() + "..."
 }
+
+fun String.stripHtml() = this
+    .replace(Regex("<[^>]*>"), "")
+    .replace(Regex("&amp;|&lt;|&gt;|&quot;|&apos;|&#\\d+;"), "")
+    .replace(Regex(" +"), " ")
